f0e1787b6837e741baf119b2fd2089e54d25a122
cylic-reference <alex@cyclicreference.space>
2016-01-31 16:46:49 -0600

    HazelcastSingleton CDI not working as anticipated, but got logging working.

--- src/main/java/space/cyclic/reference/app/rest/HazelcastAdmin.java
+++ src/main/java/space/cyclic/reference/app/rest/HazelcastAdmin.java
@@ -1,14 +1,20 @@
 package space.cyclic.reference.app.rest;
 
+import space.cyclic.reference.beans.HazelcastSingleton;
+
+import javax.ejb.EJB;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import java.util.concurrent.ExecutionException;
 
 @Path("/admin")
 public class HazelcastAdmin {
+    @EJB
+    HazelcastSingleton hazelcastSingleton;
 
     @GET
     @Path("/get/thing")
-    public String getThing(){
-        return "thing works";
+    public String getThing() throws ExecutionException, InterruptedException {
+        return hazelcastSingleton.getThing().get();
     }
 }
--- src/main/java/space/cyclic/reference/beans/HazelcastSingleton.java
+++ src/main/java/space/cyclic/reference/beans/HazelcastSingleton.java
@@ -1,11 +1,51 @@
 package space.cyclic.reference.beans;
 
+import com.hazelcast.config.ClasspathXmlConfig;
+import com.hazelcast.config.Config;
+import com.hazelcast.config.ExecutorConfig;
+import com.hazelcast.config.MapConfig;
+import com.hazelcast.core.Hazelcast;
+import com.hazelcast.core.HazelcastInstance;
+import org.apache.log4j.Logger;
+
+import javax.annotation.PostConstruct;
+import javax.ejb.AsyncResult;
+import javax.ejb.Asynchronous;
 import javax.ejb.Startup;
 import javax.inject.Singleton;
+import java.util.concurrent.Future;
 
 @Startup
 @Singleton
 public class HazelcastSingleton {
+    private static Logger logger = Logger.getLogger(HazelcastSingleton.class);
+    HazelcastInstance hazelcastMember;
 
+    @PostConstruct
+    public void startHazelcastNode(){
+        Config hazelcastConfig = new ClasspathXmlConfig("hazelcast-client.xml");
 
+        ExecutorConfig executorConfig = new ExecutorConfig()
+                .setName("space.cyclic.reference.bestExecutor")
+                .setPoolSize(15);
+        hazelcastConfig.addExecutorConfig(executorConfig);
+
+        //Wildcard Configurations
+        MapConfig mapConfig = new MapConfig()
+                .setName("space.cyclic.reference.bestMap*")
+                .setBackupCount(1)
+                .setTimeToLiveSeconds(0);
+
+        hazelcastConfig.addMapConfig(mapConfig);
+
+        hazelcastConfig.setProperty("hazelcast.logging.type", "log4j");
+
+        hazelcastMember = Hazelcast.newHazelcastInstance(hazelcastConfig);
+        logger.warn("Member Initialized");
+    }
+
+    @Asynchronous
+    public Future<String> getThing() {
+        return new AsyncResult<>("hazelcast thing works");
+    }
 }
