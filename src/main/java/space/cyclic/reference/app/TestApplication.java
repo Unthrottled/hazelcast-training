f7d2c95ed8198bc5f9a4139865ba38c9415c3e9d
cylic-reference <alex@cyclicreference.space>
2016-01-30 21:39:47 -0600

    Forgot to add these?

--- src/main/java/space/cyclic/reference/app/TestApplication.java
+++ src/main/java/space/cyclic/reference/app/TestApplication.java
@@ -1,7 +1,20 @@
 package space.cyclic.reference.app;
 
-/**
- * Created by pidgeonsquish on 1/30/16.
- */
-public class TestApplication {
+import space.cyclic.reference.app.rest.Test;
+
+import java.util.HashSet;
+import java.util.Set;
+import javax.ws.rs.core.Application;
+
+public class TestApplication extends Application {
+    private Set<Object> singletons = new HashSet<Object>();
+
+    public TestApplication() {
+        singletons.add(new Test());
+    }
+
+    @Override
+    public Set<Object> getSingletons() {
+        return singletons;
+    }
 }
